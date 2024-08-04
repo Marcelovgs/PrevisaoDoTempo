6d567c8ce12114c630dd171e4d2ba0734e16d5a6
Marcelovgs <marcelovgs12@gmail.com>
2024-08-04 19:25:35 -0300

    atualizaçao

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import { useState, useRef } from 'react'
 import axios from 'axios'
 import './App.css'
 import WeatherInformations from './components/WeatherInformations/Weatherinformations'
+import WeatherInformations5Days from './components/WeatherInformations5Days/WeatherInformations5Days'
 
 
 function App() {
   const [weather, setWeather] = useState()
+  const [weather5Days, setWeather5Days] = useState()
+
+
   const inputRef = useRef()
 
   async function searchCity() {
@@ -13,8 +17,12 @@ function App() {
     const city = inputRef.current.value
     const key = "f845468909187420adbfd479be072dad"
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`
+    const url5Days = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}&lang=pt_br&units=metric`
 
     const apiInfo = await axios.get(url)
+    const apiInfo5Days = await axios.get(url5Days)
+
+    setWeather5Days(apiInfo5Days.data)
     setWeather(apiInfo.data)
 
 
@@ -22,11 +30,12 @@ function App() {
 
   return (
     <div className='container'>
-      <h1>DevClub Previsão do tempo</h1>
+      <h1>Previsão do tempo</h1>
       <input ref={inputRef} type='text' placeholder='Digite o nome da cidade' />
       <button onClick={searchCity}>Buscar</button>
 
       {weather && <WeatherInformations weather={weather} />}
+      {weather5Days && <WeatherInformations5Days weather5Days={weather5Days} />}
 
     </div>
   );
---
+++ src/components/WeatherInformations5Days/WeatherInformations5Days.jsx
@@ -0,0 +1,16 @@
+import './WeatherInformations5Days.css'
+
+function WeatherInformations5Days({ weather5Days }) {
+    console.log(weather5Days)
+
+    return (
+        <div className='weather-container'>
+            <p>5days</p>
+        </div>
+
+
+    )
+
+}
+
+export default WeatherInformations5Days
