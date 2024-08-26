e5107f9cae3fc94ed5048d9a1f3d6e17880f3670
Marcelovgs <marcelovgs12@gmail.com>
2024-08-26 14:43:15 -0300

    first commit

--- src/components/WeatherInformations5Days/WeatherInformations5Days.jsx
+++ src/components/WeatherInformations5Days/WeatherInformations5Days.jsx
@@ -24,67 +24,27 @@ function WeatherInformations5Days({ weather5Days }) {
     const nextFiveDays = Object.values(dailyForecast).slice(1, 6);
 
     function convertDate(date) {
-        const newDate = new Date(date.dt * 1000).toLocaleDateString('pt-BR', { weekday: 'long', day: '2-digit' });
-
-        return newDate;
-    }
-
-    let dailyForecast = {}
-
-    for (let forecast of weather5Days.list) {
-        const date = new Date(forecast.dt * 1000).toLocaleDateString()
-
-        if (!dailyForecast[date]) {
-            dailyForecast[date] = {
-                minTemp: forecast.main.temp_min,
-                maxTemp: forecast.main.temp_max,
-                weather: forecast.weather[0],
-                dt: forecast.dt
-            }
-        } else {
-            dailyForecast[date].minTemp = Math.min(dailyForecast[date].minTemp, forecast.main.temp_min)
-            dailyForecast[date].maxTemp = Math.max(dailyForecast[date].maxTemp, forecast.main.temp_max)
-        }
-    }
-
-    const nextFiveDays = Object.values(dailyForecast).slice(1, 6)
-
-    function convertDate(date) {
-        const newDate = new Date(date.dt * 1000).toLocaleDateString('pt-BR', { weekday: 'long', day: '2-digit' })
-
-        return newDate
+        return new Date(date.dt * 1000).toLocaleDateString('pt-BR', { weekday: 'long', day: '2-digit' });
     }
 
     return (
-        <div className='weather-container'>
-<<<<<<< HEAD
-            <h3>Previsão para os Próximos 5 dias</h3>
-=======
-            <h3>Previsão para os Proximos 5 dias.</h3>
->>>>>>> 49b0ac8 (Melhorando visualmente)
+        <div className="weather-container">
+            <h3>Previsão Próximos 5 Dias</h3>
             <div className='weather-list'>
                 {nextFiveDays.map(forecast => (
-                    <div key={forecast.dt} className='weather-item'>
-                        <p className='forecast-day'>{convertDate(forecast)}</p>
-                        <img src={`http://openweathermap.org/img/wn/${forecast.weather.icon}.png`} alt={forecast.weather.description} />
-                        <p className='forecast-description'>{forecast.weather.description}</p>
-<<<<<<< HEAD
-                        <p>{Math.round(forecast.minTemp)}ºC Min / {Math.round(forecast.maxTemp)}ºC Max</p>
-=======
-                        <p>{Math.round(forecast.minTemp)}ºC Min / {Math.round(forecast.maxTemp)} ºC Max</p>
->>>>>>> 49b0ac8 (Melhorando visualmente)
+                    <div key={forecast.dt} className="weather-item">
+                        <p>{convertDate(forecast)}</p>
+                        <img
+                            src={`http://openweathermap.org/img/wn/${forecast.weather.icon}.png`}
+                            alt="weather icon"
+                        />
+                        <p>{forecast.weather.description}</p>
+                        <p>{Math.round(forecast.minTemp)}°C min / {Math.round(forecast.maxTemp)}°C max</p>
                     </div>
                 ))}
             </div>
         </div>
-<<<<<<< HEAD
     );
 }
 
 export default WeatherInformations5Days;
-=======
-    )
-}
-
-export default WeatherInformations5Days
->>>>>>> 49b0ac8 (Melhorando visualmente)
